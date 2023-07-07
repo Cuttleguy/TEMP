831ae058bb8d30793444fe44c3abd6c2c37f752b
SciTechamitaitelrav <138043202+SciTechamitaitelrav@users.noreply.github.com>
2023-07-07 12:22:34 -0400

    FORMPART

--- config/db.php
+++ config/db.php
@@ -8,8 +8,8 @@ try {
     $pdo= new PDO($dsn,$username,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-    // $pdo->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('DBStatement',array($pdo)));
-    echo("YOU HAVE CONNECTED");
+
+   
 } catch (PDOException $e) {
     $errorMessage=$e->getMessage();
     echo $errorMessage;
--- home.php
+++ home.php
@@ -4,10 +4,23 @@
     $stmt=$pdo->prepare("SELECT * FROM users WHERE Id=?");
     $stmt->execute(["1"]);
     $user=$stmt->fetch();
-    echo($user->email);
+    // echo($user->name);
     
 ?>
 <p class="red">THIS TEXT IS RED</p>
+<div class="container">
+    <div class="card">
+<div class="card-header">HEADER</div>
+<div class="card-body">
+    <form action="home.php" method="post">
+        <div class="form-group">
+            <label for="reqemail">Email</label>
+            <input type="email" name="reqemail" id="" require class="form-control">
+        </div>
+    </form>
+</div>
+    </div>
+</div>
 <?php
     require("footer.php");
 ?>
